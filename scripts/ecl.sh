34de8d31e7ebe0f3a666fd4ffbcebb604f0439f4
jafarbakhshaliyev <jafar.bakhshaliyev@gmail.com>
2026-03-12 03:16:31 +0100

    all files ready.

--- scripts/ecl.sh
+++ scripts/ecl.sh
@@ -16,26 +16,6 @@ fi
 
 
 
-python maints_pred1.py \
-    --model FGN \
-    --data COVID \
-    --feature_size 55\
-    --embed_size 256 \
-    --hidden_size 512 \
-    --batch_size 4 \
-    --train_ratio 0.6 \
-    --val_ratio 0.2 \
-    --seq_length 12 \
-    --pre_length 48 \
-    --train_epochs 100 \
-    --learning_rate 0.0001 \
-    --T 4 \
-    --tau 2.0 \
-    --alpha 1.0 \
-    --device cuda:0 >logs/LongForecasting/COVID_48_FGN.log
-
-
-
 
 
 
@@ -70,7 +50,25 @@ python train.py \
     --train_epochs 100 \
     --learning_rate 0.00001 \
     --T 16 \
+    --blocks 2\
     --device cuda:0 >logs/LongForecasting/ECL_SpikF.log
+
+
+python train.py \
+    --model iSpikformer \
+    --data electricity \
+    --feature_size 370\
+    --embed_size 128 \
+    --hidden_size 256 \
+    --batch_size 16 \
+    --train_ratio 0.7 \
+    --val_ratio 0.2 \
+    --seq_length 12 \
+    --pre_length 12 \
+    --train_epochs 100 \
+    --learning_rate 0.00001 \
+    --blocks 2 \
+    --device cuda:0 >logs/LongForecasting/ECL_iSpikformer.log
 
 
 
@@ -109,6 +107,80 @@ python train.py \
 
 
 python train.py \
+    --model SpikeRNN_CPG \
+    --data electricity \
+    --feature_size 370\
+    --embed_size 128 \
+    --hidden_size 128\
+    --batch_size 16 \
+    --train_ratio 0.7 \
+    --val_ratio 0.2 \
+    --seq_length 12 \
+    --pre_length 12 \
+    --train_epochs 100 \
+    --learning_rate 0.00001 \
+    --blocks 2 \
+    --device cuda:0 >logs/LongForecasting/ECL_SpikeRNNCPG.log
+
+
+
+
+python train.py \
+    --model SpikeGRU \
+    --data electricity \
+    --feature_size 370\
+    --embed_size 128 \
+    --hidden_size 64 \
+    --batch_size 16 \
+    --train_ratio 0.7 \
+    --val_ratio 0.2 \
+    --seq_length 12 \
+    --pre_length 12 \
+    --train_epochs 100 \
+    --learning_rate 0.00001 \
+    --device cuda:0 >logs/LongForecasting/ECL_SpikeGRU.log
+
+
+
+
+python train.py \
+    --model SpikeTCN_CPG \
+    --data electricity \
+    --feature_size 370\
+    --embed_size 128 \
+    --hidden_size 64\
+    --batch_size 16 \
+    --train_ratio 0.7 \
+    --val_ratio 0.2 \
+    --seq_length 12 \
+    --pre_length 12 \
+    --train_epochs 100 \
+    --learning_rate 0.00001 \
+    --blocks 3\
+    --device cuda:0 >logs/LongForecasting/ECL_SpikeTCNCPG.log
+
+
+
+
+python train.py \
+    --model Spikformer_CPG \
+    --data electricity \
+    --feature_size 370\
+    --embed_size 128 \
+    --hidden_size 128\
+    --batch_size 16 \
+    --train_ratio 0.7 \
+    --val_ratio 0.2 \
+    --seq_length 12 \
+    --pre_length 12 \
+    --train_epochs 100 \
+    --learning_rate 0.00001 \
+    --blocks 2 \
+    --device cuda:0 >logs/LongForecasting/ECL_SpikformerCPG.log
+
+
+
+python train.py \
     --model TSTCN \
     --data electricity \
     --feature_size 370\
@@ -121,5 +193,40 @@ python train.py \
     --pre_length 12 \
     --train_epochs 100 \
     --learning_rate 0.00001 \
+    --kernel_size 3\
     --blocks 3 \
     --device cuda:0 >logs/LongForecasting/ECL_TSTCN.log
+
+
+python train.py \
+    --model TSGRU \
+    --data electricity \
+    --feature_size 370\
+    --embed_size 128 \
+    --hidden_size 64 \
+    --batch_size 16 \
+    --train_ratio 0.7 \
+    --val_ratio 0.2 \
+    --seq_length 12 \
+    --pre_length 12 \
+    --train_epochs 100 \
+    --learning_rate 0.00001 \
+    --device cuda:0 >logs/LongForecasting/ECL_TSGRU.log
+
+
+
+
+python train.py \
+    --model TSFormer \
+    --data electricity \
+    --feature_size 370\
+    --embed_size 128 \
+    --hidden_size 64 \
+    --batch_size 16 \
+    --train_ratio 0.7 \
+    --val_ratio 0.2 \
+    --seq_length 12 \
+    --pre_length 12 \
+    --train_epochs 100 \
+    --learning_rate 0.00001 \
+    --device cuda:0 >logs/LongForecasting/ECL_TSFormer.log
